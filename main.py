b5d598b76a9cf1b82ec8d1cffa7e53537bdf771b
HelloRocket <justin@hellorocket.org>
2020-10-20 17:54:06 +0900

    Update for class

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-basic.show_string("Hello World!")
+def on_button_pressed_a():
+    basic.show_string("H")
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+basic.show_string("Hello World!", 20)
+basic.show_icon(IconNames.HEART)
+basic.show_number(0)
+basic.show_leds("""
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    """)
--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-basic.showString("Hello World!")
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.showString("H")
+})
+basic.showString("Hello World!", 20)
+basic.showIcon(IconNames.Heart)
+basic.showNumber(0)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
